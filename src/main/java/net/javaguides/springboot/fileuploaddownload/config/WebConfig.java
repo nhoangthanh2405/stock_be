56a0752b72468107e19ddf1726ec7114101db1a8
nhoangthanh <nguyenhoangthanh2405@gmail.com>
2024-08-27 22:33:04 +0700

    update web config

--- src/main/java/net/javaguides/springboot/fileuploaddownload/config/WebConfig.java
+++ src/main/java/net/javaguides/springboot/fileuploaddownload/config/WebConfig.java
@@ -1,22 +1,26 @@
 package net.javaguides.springboot.fileuploaddownload.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("http://localhost:4200",
-            "https://stock-fe-nhoangthanh.vercel.app",
-            "http://stock-fe-nhoangthanh.vercel.app")  // Các origin mà bạn muốn cho phép
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
-        .allowedHeaders("*")
-        .allowCredentials(true);  // Cho phép sử dụng credentials (cookie, authorization header)
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:4200",
+                "http://stock-fe-nhoangthanh.vercel.app",
+                "https://stock-fe-nhoangthanh-tma.vercel.app") // Thay thế bằng miền frontend của bạn
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
   }
 }
